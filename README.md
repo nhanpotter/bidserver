b72e8e618cc590304f5522a1176a455a275b2171
nhanpotter <nhanpotter8a@gmail.com>
2021-01-29 16:29:19 +0800

    create models

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # bid-server
+
+```shell
+# Go to project root
+# Create virtual env
+python3 -m venv .venv
+# Activate venv
+source .venv/bin/activate
+# Install dependencies
+pip3 install -r requirements.txt
+# Migrate
+./manage.py migrate
+```
---
+++ bidapp/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils import timezone
+
+from .constants import DEFAULT_TOKEN_THRESHOLD, TOKEN_PER_DAY
+
+
+# Create your models here.
+class User(models.Model):
+    userid = models.CharField(primary_key=True, max_length=50)
+    # attribute
+    token = models.PositiveIntegerField(default=TOKEN_PER_DAY)
+
+
+class Shop(models.Model):
+    shopid = models.CharField(primary_key=True, max_length=50)
+    # attribute
+    accumulate_token = models.PositiveIntegerField(default=0)
+
+
+class BidItem(models.Model):
+    itemid = models.CharField(primary_key=True, max_length=50)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    # attribute
+    token_threshold = models.PositiveIntegerField(default=DEFAULT_TOKEN_THRESHOLD)
+    release_date = models.DateField()
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class BidTransaction(models.Model):
+    item = models.ForeignKey(BidItem, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # attribute
+    token_bid = models.PositiveIntegerField()
+    create_time = models.DateTimeField(default=timezone.now)
