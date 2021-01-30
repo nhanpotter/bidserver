8041241e9d1a3ec1132c23d0f9fe6e775b7333b6
nhanpotter <nhanpotter8a@gmail.com>
2021-01-30 21:37:37 +0800

    add urls new api

--- bidapp/urls.py
+++ bidapp/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 
 from .views import ShopBidItemCreateAPIView, ShopBidItemEditAPIView, ShopViewShopBidItemAPIView, ShopViewTokenAPIView, \
     UserProposeBidAPIView, UserViewBidItemAPIView, UserViewBidItemPersonalAPIView, UserViewPerBidItemAPIView, \
-    UserViewShopBidItemAPIView, UserViewTokenAPIView, UserViewWinItemAPIView
+    UserViewShopBidItemAPIView, UserViewTokenAPIView, UserViewWinItemAPIView, ShopViewAllAPIView, ShopViewItemAPIView, \
+    UserNotificationAPIView
 
 urlpatterns = [
     path('shop/token/', ShopViewTokenAPIView.as_view(), name='shop_token'),
     path('shop/view/', ShopViewShopBidItemAPIView.as_view(), name='shop_view_shop_item'),
     path('shop/create/', ShopBidItemCreateAPIView.as_view(), name='shop_create'),
     path('shop/edit/', ShopBidItemEditAPIView.as_view(), name='shop_edit'),
+    path('shop/view_all/', ShopViewAllAPIView.as_view(), name='shop_view_all'),
+    path('shop/view_item/', ShopViewItemAPIView.as_view(), name='shop_view_item'),
 
     path('user/token/', UserViewTokenAPIView.as_view(), name='user_token'),
     path('user/view_by_shop/', UserViewShopBidItemAPIView.as_view(), name='user_view_shop_item'),
@@ -17,4 +20,6 @@ urlpatterns = [
     path('user/view_all/', UserViewBidItemAPIView.as_view(), name='user_view_item'),
     path('user/view_all_personal/', UserViewBidItemPersonalAPIView.as_view(), name='user_view_all_personal'),
     path('user/view_per_item/', UserViewPerBidItemAPIView.as_view(), name='user_view_per_item'),
+
+    path('user/notification/', UserNotificationAPIView.as_view(), name='notification'),
 ]
