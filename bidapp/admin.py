ca941553d40d17ff9c01e68bf82c0b50b971a91d
nhanpotter <nhanpotter8a@gmail.com>
2021-01-30 21:32:12 +0800

    add notification to admin site

--- bidapp/admin.py
+++ bidapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import BidItem, User, Shop, BidTransaction
+from .models import BidItem, User, Shop, BidTransaction, Notification
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Shop)
 admin.site.register(BidItem)
 admin.site.register(BidTransaction)
+admin.site.register(Notification)
